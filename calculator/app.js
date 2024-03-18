daf7399cad8e28e0be044f551a667c9d7ab9fbfa
azinaaitbai <ajtbajazina@gmail.com>
2024-03-18 14:06:34 +0600

    a

---
+++ calculator/app.js
@@ -0,0 +1,67 @@
+const resultElement = document.getElementById("result");
+const input1 = document.getElementById("input1");
+const input2 = document.getElementById("input2");
+const submitBtn = document.getElementById("submit");
+const plusBtn = document.getElementById("plus");
+const minusBtn = document.getElementById("minus");
+const multipleBtn = document.getElementById("multiple");
+const divideBtn = document.getElementById("divide");
+let action = "+";
+
+console.log(input1.value);
+console.log(input2.value);
+let result = 0;
+
+plusBtn.onclick = function () {
+  action = "+";
+  console.log(action);
+};
+
+minusBtn.onclick = function () {
+  action = "-";
+  console.log(action);
+};
+
+multipleBtn.onclick = function () {
+  action = "*";
+  console.log(action);
+};
+
+divideBtn.onclick = function () {
+  action = "/";
+  console.log(action);
+};
+
+submitBtn.onclick = function () {
+  if (action == "+") {
+    result = Number(input1.value) + Number(input2.value);
+    console.log(result);
+  }
+  if (action == "-") {
+    result = Number(input1.value) - Number(input2.value);
+    console.log(result);
+  }
+  if (action == "*") {
+    result = Number(input1.value) * Number(input2.value);
+    console.log(result);
+  }
+  if (action == "/") {
+    result = Number(input1.value) / Number(input2.value);
+    console.log(result);
+  }
+  addColor(result);
+  //if (result < 0) {
+  //  resultElement.style.color = "red";
+  //} else {
+  //  resultElement.style.color = "green";
+  //}
+  resultElement.textContent = result;
+};
+
+function addColor(result) {
+  if (result < 0) {
+    resultElement.style.color = "red";
+  } else {
+    resultElement.style.color = "green";
+  }
+}
